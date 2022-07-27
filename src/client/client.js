599cb122eb7548671ee24b48e8e77121b407058d
Owoade <63651250+Owoade@users.noreply.github.com>
2022-07-27 03:58:02 +0100

    Implemented TDD with mocha and chai

--- src/client/client.js
+++ src/client/client.js
@@ -1,23 +1,23 @@
 
-/* Ensure you have the socket io script loaded on the dom before this script */
-function RequestClient( host ) {
-    if(!io){
-        console.error("The socket.io client is missing")
-    }
-    const socket = io( host )
-    return {
-        req(event) {
-            return new Promise((res, rej) => {
-                socket.emit(event);
-                socket.on(event, (response) => {
-                    res(response);
-                    return
-                })
-                socket.on("no-listener-attached", (err) => {
-                    rej(err)
-                })
-            })
-        }
-    }
+// /* Ensure you have the socket io script loaded on the dom before this script */
+// function RequestClient( host ) {
+//     if(!io){
+//         console.error("The socket.io client is missing")
+//     }
+//     const socket = io( host )
+//     return {
+//         req(event) {
+//             return new Promise((res, rej) => {
+//                 socket.emit(event);
+//                 socket.on(event, (response) => {
+//                     res(response);
+//                     return
+//                 })
+//                 socket.on("no-listener-attached", (err) => {
+//                     rej(err)
+//                 })
+//             })
+//         }
+//     }
 
-}
+// }
