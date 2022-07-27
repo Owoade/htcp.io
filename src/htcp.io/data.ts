7ba082637c95300e270c9ceffcc44b1a75366019
Owoade <63651250+Owoade@users.noreply.github.com>
2022-07-27 04:29:08 +0100

    Implemented TDD with mocha and chai

---
+++ src/htcp.io/data.ts
@@ -0,0 +1,17 @@
+export interface IUser {
+    name: string;
+    user: string;
+    id: number;
+}
+
+export let users = [
+    {
+        name: "Makinde mayowa ",
+        user: "bianca123@gmail.com", id: 1
+    },
+    {
+        name: "Olaoye Seyi Favour",
+        user: "busolaajao123@gmail.com",
+        id: 2
+    }
+]
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,41 @@
 import htcpio from "./htcp.io";
+import { users, type IUser } from "./htcp.io/data";
 
 const { listen, req } = htcpio();
 
-req("get-status", ()=>{
+req("get-status", () => {
   return true
 })
 
-req("get-users", ()=>{
-    return [ "makinde", "segun", "tayo"]
+req("get-users", () => {
+  return users
 })
 
-listen( 3000, "Server has connected")
+req("add-user", ( user: IUser  )=>{
+  users.push({...user, id: Date.now() });
+  return {
+    user,
+    message: "User added successfully"
+  }
+})
+
+req( "update-user", ({id, update}:{id: number, update: Partial<IUser>})=>{
+  const user = users.find( _ => _.id === id) as IUser 
+  if( !user ) return "user not found"
+
+   users.filter( _ => _.id !== id).concat( [{ ...user, ...update }] );
+
+  return { ...user, ...update }
+
+})
+
+req("delete-user", (id) => {
+   users.filter( _ => _.id !== id ) 
+  return { user: users.find( _ => _.id === id), message: "User deleted successfully" }
+})
+
+
+
+
+
+listen(3000, "Server has connected")
